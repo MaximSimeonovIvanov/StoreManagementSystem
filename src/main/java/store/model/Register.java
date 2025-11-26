3727bbea038005d0d7eb01bf343807c0be76eb6e
MaximSimeonovIvanov <maxim.s.ivanoff@gmail.com>
2025-11-26 10:04:00 +0200

    simple register model. TODO: add validations

--- src/main/java/store/model/Register.java
+++ src/main/java/store/model/Register.java
@@ -1,4 +1,19 @@
 package store.model;
 
 public class Register {
+    private int id;
+    private String name;
+
+    public Register(int id,String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
