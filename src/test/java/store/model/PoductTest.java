45aa1396e50d56d23a07cbc1fbb084e9a4371a28
MaximSimeonovIvanov <maxim.s.ivanoff@gmail.com>
2026-04-06 12:55:04 +0300

    first tests

--- src/test/java/store/model/PoductTest.java
+++ src/test/java/store/model/PoductTest.java
@@ -13,5 +13,32 @@ public class PoductTest {
     @BeforeEach
     void setUp(){
         //sazdava nov product predi vseki test
+        product = new Product(1, "Мляко", 2.00,
+                LocalDate.now().plusDays(10), ProductCategory.FOOD);
+    }
+
+    @Test
+    void testProductCreationValid() {
+        assertEquals(1, product.getId());
+        assertEquals("Мляко", product.getName());
+        assertEquals(2.00, product.getSupplyPrice());
+        assertEquals(ProductCategory.FOOD, product.getCategory());
+        assertEquals(0, product.getQuantityDelivered());
+        assertEquals(0, product.getQuantitySold());
+        assertEquals(0, product.getAvailableQuantity());
+    }
+
+    @Test
+    void testProductCreationThrowsWhenIdIsZeroOrNegative() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Product(0, "Грешка", 1.0, LocalDate.now(), ProductCategory.FOOD));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Product(-5, "Грешка", 1.0, LocalDate.now(), ProductCategory.FOOD));
+    }
+
+    @Test
+    void testProductCreationThrowsWhenNameIsNull() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Product(2, null, 1.0, LocalDate.now(), ProductCategory.FOOD));
     }
 }
