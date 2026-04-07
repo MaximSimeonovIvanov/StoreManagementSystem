353bbec71cd46f0be4c7eb3ee830cbdc27697c8f
MaximSimeonovIvanov <maxim.s.ivanoff@gmail.com>
2026-04-07 16:23:32 +0300

    get all cashiers test

--- src/test/java/store/service/CashierServiceImplTest.java
+++ src/test/java/store/service/CashierServiceImplTest.java
@@ -52,4 +52,18 @@ public class CashierServiceImplTest {
         Cashier found = cashierService.findCashierById(9999);
         assertNull(found);
     }
+
+    @Test
+    void testGetAllCashiers() {
+        Cashier cashier1 = new Cashier(1, "Иван", 1500.0);
+        Cashier cashier2 = new Cashier(2, "Георги", 1400.0);
+        cashierService.addCashier(cashier1);
+        cashierService.addCashier(cashier2);
+
+        List<Cashier> all = cashierService.getAllCashiers();
+        assertEquals(2, all.size());
+        //списъкът е копиe
+        assertThrows(UnsupportedOperationException.class,
+                () -> all.add(new Cashier(3, "Тест", 1000.0)));
+    }
 }
